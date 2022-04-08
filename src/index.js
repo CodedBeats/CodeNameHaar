893fa70a904e03bd4b8a4f68d7c8831fc9eb2b98
Cook <luca.haar@icloud.com>
2022-04-08 18:09:44 +1000

    added firebase connection and a way to read what currently in the server, (should make a serperate file fore firebase stuff

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore, collection, getDocs } from "firebase/firestore"
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdbkOeREKjkTZwnZXQNoKPn9wtAUEGTMI",
+  authDomain: "codedbeats-47589.firebaseapp.com",
+  projectId: "codedbeats-47589",
+  storageBucket: "codedbeats-47589.appspot.com",
+  messagingSenderId: "927207907473",
+  appId: "1:927207907473:web:9997198672de5f2efc9f30"
+};
+
+// init firebase app
+initializeApp(firebaseConfig)
+// init services
+const db = getFirestore()
+// collection ref
+const colRef = collection(db, "woods")
+// get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+  // console.log(snapshot.docs)
+  let woods = []
+  snapshot.docs.forEach((doc) => {
+    woods.push({ ...doc.data(), id: doc.id })
+  })
+  console.log(woods)
+  })
+  .catch((err) => {
+    console.error(err.message)
+  })
+
 
 ReactDOM.render(
   <React.StrictMode>
