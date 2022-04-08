70c701ee221b67ddd0c755c670e55bba1debe30f
Cook <luca.haar@icloud.com>
2022-04-08 17:31:14 +1000

    clean slate

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
-import Navbar from "./components/nav/Navbar";
-import Dashboard from './components/dashboard/Dashboard';
-import Create from "./components/blog/Create";
-import BlogDetails from "./components/blog/BlogDetails"
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import NotFound from "./components/errors/NotFound";
 
-function App() {
+const App = () => {
+    // return ( 
 
-    return (
-        <Router>
-            <div className="App">
-                <Navbar />
-                <div className="content">
-                    <Switch>
-                        <Route exact path="/">
-                            <Dashboard />
-                        </Route>
-                        <Route path="/create">
-                            <Create />
-                        </Route>
-                        <Route path="/blogs/:id">
-                            <BlogDetails />
-                        </Route>
-                        {/* this route goes at the bottom because "*" will catch all other routes, that way we catch the errors */}
-                        <Route path="*">
-                            <NotFound />
-                        </Route>
-                    </Switch>
-                </div>
-            </div>
-        </Router>
-    );
+    // );
 }
-
+ 
 export default App;
