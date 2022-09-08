0cb247501328af6e30e23544007193e8da712cc6
Luca <luca.haar@icloud.com>
2022-09-08 22:12:50 +1000

    page link -- added a button to dashboard which uses 'Link' from react-router to redirect to add post page

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,7 +1,17 @@
+// dependencies
+import { Link } from "react-router-dom"
+
 const Dashboard = () => {
     
     return ( 
-        <h1>Title</h1>
+        <div>
+            <button>
+                <Link to="../posts/add">x</Link>
+            </button>
+
+
+            <h1>Title</h1>
+        </div>
     );
 }
  
