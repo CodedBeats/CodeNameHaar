ff73463d2287f851916fe7cac8273bed085cc226
Luca <luca.haar@icloud.com>
2022-12-15 11:13:51 +1100

    lab cursor comments -- added comments to the fancy cursor code to explain whats happening (and added an update to be made for better efficiency)

--- src/components/lab/Cursors.js
+++ src/components/lab/Cursors.js
@@ -3,47 +3,51 @@ import "./css/cursors.css"
 
 // dependencies
 
-
+// == This can be improved with props and onMouseMove as func rather than global and always activating on this page == //
 export const Cursor1 = () => {
+    // set initial coords
     const coords = { x: 0, y: 0 };
     const circles = document.querySelectorAll(".cursor1-circle");
-
+    // set possition of circle divs at initial cords
     circles.forEach(function (circle) {
         circle.x = 0;
         circle.y = 0;
     });
-
+    // get coords of mouse possition
     window.addEventListener("mousemove", function (e) {
         coords.x = e.clientX;
         coords.y = e.clientY;
     });
-
+    // handle animation
     function animateCircles() {
+        // get coords
         let x = coords.x;
         let y = coords.y;
-
+        // update every circle
         circles.forEach(function (circle, index) {
+            // align circle with cursor
             circle.style.left = x - 12 + "px";
             circle.style.top = y - 12 + "px";
-
+            // circle gets progressivley smaller as train goes
             circle.style.scale = (circles.length - index) / circles.length;
-
+            // assign coords to circle poss
             circle.x = x;
             circle.y = y;
-
+            // set following circles
             const nextCircle = circles[index + 1] || circles[0];
-            x += (nextCircle.x - x) * 0.4;
+            x += (nextCircle.x - x) * 0.4; // change these values to increase tail length
             y += (nextCircle.y - y) * 0.4;
         });
         requestAnimationFrame(animateCircles);
     }
     animateCircles();
-
+    // hide fancy cursor when leaving div
     function hideTrail() {
         circles.forEach(function (circle) {
             circle.style.display = "none"
         });
     }
+    // show fancy cursor when entering div
     function showTrail() {
         circles.forEach(function (circle) {
             circle.style.display = "block"
