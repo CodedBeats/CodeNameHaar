cb6c4be6bb16f1b62d2737ef9ac861832ddee90d
Luca <luca.haar@icloud.com>
2022-12-20 08:23:43 +1100

    animated title -- made a small possible way to animate the landing title (but I don't like it some I'll do a different version)

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 // dependencies
-// import { useState } from "react"
+import { useState } from "react"
 
 // style
 import ".././misc/css/page-container.css"
@@ -11,6 +11,8 @@ import Footer from "../nav/Footer"
 
 
 const Dashboard = () => {
+
+    const [title, setTitle] = useState("<")
     
     return ( 
         <div className="dashboard container">
@@ -19,8 +21,14 @@ const Dashboard = () => {
             <div className="page-container">
                 <div className="body-container">
                     <div className="synopsis-container">
+                        {/* <div className="synopsis-title" onMouseOver={() => setTitle("<CodedBeats")}> */}
+                            {/* {title}  */}
                         <div className="synopsis-title">
-                            Luca Haar
+                            &lt;CodedBeats /&gt;
+                        </div>
+                        <div className="synopsis-content">
+                            Hi there, I'm Luca Haar. <br />
+                            Im a Website Develper working in Melbourne Australia.
                         </div>
 
                     </div>
